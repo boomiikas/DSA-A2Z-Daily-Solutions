3ce2cfae90c23dd12d30e932abe5fc800dd60f88
boomikas <boomikas2007@gmail.com>
2025-09-04 15:56:33 +0530

    Day10

---
+++ Day10/alldivisors.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+divisors = set()
+for i in range(1, int(n**0.5) + 1):
+    if n % i == 0:
+        divisors.add(i)
+        divisors.add(n // i)
+
+divisors = sorted(divisors)
+print(divisors)
